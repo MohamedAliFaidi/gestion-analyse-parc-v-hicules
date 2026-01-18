96bba198490a1d8363dcb6ea71325375436d5c46
MohamedAliFaidi <mouhammedalifaidi@icloud.com>
2026-01-18 11:03:45 +0100

    cleanup

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
 #include "vehicule.h"
+#include <stdio.h>
 
 #define MAX_VEHICULES 100
 
 int main() {
-    int n;
-    Vehicule parc[MAX_VEHICULES];
-    int idxMeilleur, idxMoinsRentable;
+  int n;
+  Vehicule parc[MAX_VEHICULES];
+  int idxMeilleur, idxMoinsRentable;
 
-    do {
-        printf("Nombre de véhicules à saisir (1-%d): ", MAX_VEHICULES);
-        scanf("%d", &n);
-    } while(n <= 0 || n > MAX_VEHICULES);
+  do {
+    printf("Nombre de véhicules à saisir (1-%d): ", MAX_VEHICULES);
+    scanf("%d", &n);
+  } while (n <= 0 || n > MAX_VEHICULES);
 
-    Saisie_Vehicules(parc, n);
+  Saisie_Vehicules(parc, n);
 
-    Calcul_Benefices(parc, n);
+  Calcul_Benefices(parc, n);
 
-    Analyse_Performance(parc, n, &idxMeilleur, &idxMoinsRentable);
+  Analyse_Performance(parc, n, &idxMeilleur, &idxMoinsRentable);
 
-    printf("\n--- Liste des véhicules par bénéfice décroissant ---\n");
-    Affiche_Vehicules(parc, n);
+  printf("\n--- Liste des véhicules par bénéfice décroissant ---\n");
+  Affiche_Vehicules(parc, n);
 
-    printf("\nVéhicule le plus rentable:\n");
-    Affiche_Vehicule(parc[idxMeilleur]);
+  printf("\nVéhicule le plus rentable:\n");
+  Affiche_Vehicule(parc[idxMeilleur]);
 
-    printf("Véhicule le moins rentable:\n");
-    Affiche_Vehicule(parc[idxMoinsRentable]);
+  printf("Véhicule le moins rentable:\n");
+  Affiche_Vehicule(parc[idxMoinsRentable]);
 
-    printf("\nBénéfice total: %.2f euros\n", Benefice_Total(parc, n));
+  printf("\nBénéfice total: %.2f euros\n", Benefice_Total(parc, n));
 
-    return 0;
+  return 0;
 }
--- vehicule.h
+++ vehicule.h
@@ -4,13 +4,13 @@
 #define MAX_STRING 50
 
 typedef struct {
-    char immatriculation[MAX_STRING];
-    char marque[MAX_STRING];
-    int annee;
-    double prixParJour;
-    int joursLoues;
-    double coutMaintenance;
-    double benefice;
+  char immatriculation[MAX_STRING];
+  char marque[MAX_STRING];
+  int annee;
+  double prixParJour;
+  int joursLoues;
+  double coutMaintenance;
+  double benefice;
 } Vehicule;
 
 // Prototypes
@@ -20,7 +20,8 @@ double Revenu_Vehicule(Vehicule v);
 double Benefice_Vehicule(Vehicule v);
 void Calcul_Benefices(Vehicule parc[], int n);
 double Benefice_Total(Vehicule parc[], int n);
-void Analyse_Performance(Vehicule parc[], int n, int *idxMeilleur, int *idxMoinsRentable);
+void Analyse_Performance(Vehicule parc[], int n, int *idxMeilleur,
+                         int *idxMoinsRentable);
 void Affiche_Vehicule(Vehicule v);
 void Affiche_Vehicules(Vehicule parc[], int n);
 void Trier_Vehicules_Par_Benefice(Vehicule parc[], int n);
